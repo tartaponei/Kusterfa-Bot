0f6d97d91943a7dfe15601d47b0efb2b21ec58f3
Sarah <sarahborretti@gmail.com>
2020-11-30 15:00:58 -0300

    Mais opções adicionadas a à listas de respostas genéricas

--- answers.py
+++ answers.py
@@ -1,4 +1,4 @@
-palavras = ["digam", "diga", "mandem", "manda", "mande", "falem", "fala", "fale"]
+palavras = ["digam ", "diga ", "mandem ", "manda ", "mande ", "falem ", "fala ", "fale "]
 
 opcoes_sugestao_pessoa = ["vc", "a bruna", "o kotaka", "sua mãe", "teu pai", "everson zoio", "super xandão", "felipe neto", "ednaldo pereira", "vagner com v", "lucas hype", "elias narguilero"]
 
@@ -8,4 +8,4 @@ opcoes_imagem = ["chorei muito", "ri muito", "adorei", "o grupo da familia vai a
 
 opcoes_video = ["muito lindo emocionante", "muito bom o meme", "vou mandar no zap", "o grupo da familia vai amar"]
 
-opcoes_generica = ["mais uma crítica cirurgica do maicon kuster"]
+opcoes_generica = ["mais uma crítica cirurgica do maicon kuster", "concordo", "discordo"]
