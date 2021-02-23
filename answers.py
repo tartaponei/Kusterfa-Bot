c4de41735581179b9cbf22527700a44637784693
sarah <sarahborretti@gmail.com>
2021-02-23 11:34:15 -0300

    Adicionado mais opções de resposta

--- answers.py
+++ answers.py
@@ -1,6 +1,6 @@
 palavras = ["digam ", "diga ", "mandem ", "manda ", "mande ", "falem ", "fala ", "fale "]
 
-opcoes_sugestao_pessoa = ["vc", "a bruna", "o kotaka", "sua mãe", "teu pai", "everson zoio", "super xandão", "felipe neto", "ednaldo pereira", "vagner com v", "lucas hype", "elias narguilero"]
+opcoes_sugestao_pessoa = ["vc", "karol conka", "a lumena", "gabriel monteiro", "o renato garcia", "a bruna", "o kotaka", "sua mãe", "teu pai", "everson zoio", "super xandão", "felipe neto", "ednaldo pereira", "vagner com v", "lucas hype", "elias narguilero"]
 
 opcoes_sugestao_generica = ["e-girl", "emo", "rinha de galo", "vc", "rinha de anão", "prender anão no teto"]
 
@@ -8,4 +8,4 @@ opcoes_imagem = ["chorei muito", "ri muito", "adorei", "o grupo da familia vai a
 
 opcoes_video = ["muito lindo emocionante", "muito bom o meme", "vou mandar no zap", "o grupo da familia vai amar"]
 
-opcoes_generica = ["mais uma crítica cirurgica do maicon kuster", "concordo", "discordo"]
+opcoes_generica = ["mais uma crítica cirurgica do maicon kuster", "concordo", "discordo", "KKKKKKKKKKKKKKKKKKKKKKKKKKK", "qual foi", "👍"]
